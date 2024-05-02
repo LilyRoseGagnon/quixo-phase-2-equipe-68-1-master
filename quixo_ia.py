545dfe09b555ddab0bbb2803d0f8e9e8d363eaff
Lily Rose Gagnon <lrgag6@ulaval.ca>
2024-05-02 11:59:32 -0400

    Phase 3 - Init. QuixoIA

---
+++ quixo_ia.py
@@ -0,0 +1,24 @@
+from quixo import Quixo
+
+
+class QuixoIA(Quixo):
+    def valider_diagonale():
+        pass
+
+    def valider_horizontale():
+        pass
+
+    def valider_verticale():
+        pass
+
+    def partie_terminée():
+        pass
+
+    def trouver_coup_gagnant():
+        pass
+
+    def trouver_coup_bloquant():
+        pass
+
+    def jouer_le_coup():
+        pass
